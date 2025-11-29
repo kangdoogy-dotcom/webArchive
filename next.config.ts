2462fa7fd4aebad9978bbc335b7766a7e2396bf8
kangdoogy-dotcom <kangdoogy@gmail.com>
2025-11-29 16:00:22 +0900

    깃헙페이지최적화

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/webArchive',
+  assetPrefix: '/webArchive',
 };
 
 export default nextConfig;
